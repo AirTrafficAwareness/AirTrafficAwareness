fd2a6d64188f1659f9e77874b97bb31c27804081
Cameron Knight <cjk8zb@mail.umkc.edu>
2018-10-21 16:00:01 -0400

    Add WebStorm project. Reformat code. Add web socket node dependency.

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,6 +1,6 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import { Routes } from "./routes";
+import {Routes} from "./routes";
 
 class App {
 
@@ -9,16 +9,16 @@ class App {
 
     constructor() {
         this.app = express();
-        this.config();        
-        this.routePrv.routes(this.app);     
-        
+        this.config();
+        this.routePrv.routes(this.app);
+
     }
 
-    private config(): void{
+    private config(): void {
         // support application/json type post data
         this.app.use(bodyParser.json());
         //support application/x-www-form-urlencoded post data
-        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(bodyParser.urlencoded({extended: false}));
     }
 
 }
