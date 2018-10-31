3f725750c42081c2afe8c7955b1abb50c27c0e27
Cameron Knight <cjk8zb@mail.umkc.edu>
2018-10-31 14:11:08 -0500

    Implement WebSocketClient Use WebSocketClient as clientListener.

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,10 +1,10 @@
 import * as express from "express";
-import {Request} from "express";
-import {Response} from "express";
+import {Request, Response} from "express";
 import {AirplaneProtocol} from "./airplaneProtocol";
 import {TcasEngine} from "./engine";
 import {ClientProtocol} from "./clientProtocol";
-import { Dump1090} from "./dump1090";
+import {Dump1090} from "./dump1090";
+import {WebSocketClient} from "./webSocketClient";
 
 class App {
 
@@ -22,7 +22,7 @@ class App {
             next();
         });
 
-       const clientListener: ClientProtocol = <any>{};
+        const clientListener: ClientProtocol = new WebSocketClient(this.app);
         const dataSource: AirplaneProtocol = new Dump1090();
         const engine = new TcasEngine();
 
--- Server/src/webSocketClient.ts
+++ Server/src/webSocketClient.ts
@@ -1,11 +1,49 @@
 import * as WebSocket from 'ws';
 import {ClientProtocol} from "./clientProtocol";
 import {AirplaneData} from "./airplane";
+import {Application} from "express";
+import * as http from "http";
 
-class WebSocketClient extends ClientProtocol {
+export class WebSocketClient extends ClientProtocol {
     private client: WebSocket;
 
+    constructor(app: Application) {
+        super();
+        const server = http.createServer(app);
+        const wss = new WebSocket.Server({ server });
+        wss.on('connection', (ws: WebSocket) => {
+
+            ws.on('message', (message: string) => {
+                console.debug('WebSocketClient received message:', message);
+
+                const json = JSON.parse(message);
+                if (json.identifier) {
+                    this.client = ws;
+                    this.onClientConnected(json.identifier);
+                    ws.send(JSON.stringify({ok: {message}}));
+                } else {
+                    ws.send(JSON.stringify({error: {message}}));
+                }
+            });
+
+            ws.send(JSON.stringify({ok: 'identify'}));
+        });
+
+        //start our server
+        const port = process.env.PORT || 8999;
+        server.listen(port, () => {
+            console.log('WebSocket server listening on port', port);
+        });
+
+    }
+
     send(airplanes: AirplaneData[]) {
-        this.client.send(JSON.stringify(airplanes));
+        if (this.client && this.client.OPEN) {
+            try {
+                this.client.send(JSON.stringify(airplanes));
+            } catch (e) {
+                console.log(e);
+            }
+        }
     }
 }
