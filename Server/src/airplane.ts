bb2f63fd812a2cf74b0fdb5ca5eddda630591fc2
Cameron Knight <cjk8zb@mail.umkc.edu>
2018-10-31 13:31:41 -0500

    Refactor and update model interfaces: Add heading to airplane interface. Add x, y points to airplane data interface.

--- Server/src/airplane.ts
+++ Server/src/airplane.ts
@@ -1,15 +1,28 @@
 
-export interface Airplane {
+export interface Coordinate {
+    latitude: number;
+    longitude: number;
+}
+
+export interface Point {
+    x: number;
+    y: number;
+}
+
+export interface Airplane extends Coordinate {
     identifier: string;
     flightNumber: string;
     groundSpeed: number;
     altitude: number;
     latitude: number;
     longitude: number;
+    heading: number;
     lastUpdateDate: number;
 }
 
-export interface AirplaneData extends Airplane{
+export interface AirplaneData extends Airplane, Point {
     distance: number;
     flightZone: string;
+    x: number;
+    y: number;
 }
