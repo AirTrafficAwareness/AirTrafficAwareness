37dc65ff32e46c82ad20692c9dacbfec56db3a4e
Cameron Knight <cjk8zb@mail.umkc.edu>
2018-11-19 23:56:51 -0600

    Replace bracket notation with dot notation.

--- Server/src/engine.ts
+++ Server/src/engine.ts
@@ -6,10 +6,7 @@ interface CallbackFunction {
 
 export class ATAEngine {
     public onGeneratedDistances: CallbackFunction = (() => {});
-    public aircraftInfoJson;
-    aircraftFlightInfo: JSON;
-    myAircraft: JSON;
-    aircraftJson: JSON;
+    myAircraft: Airplane;
     dangerDistance: number;
     warningDistance: number;
     watchDistance: number;
@@ -20,26 +17,26 @@ export class ATAEngine {
             // the haversine formula takes 2 points(latlong) and finds the distances between them.
             // generally takes around 5 ms to calculate
             var R = 6371e3; // metres
-            var φ1 = toRadians(this.myAircraft['lat1']);
-            var φ2 = toRadians(aircraft['lat2']);
-            var Δφ = toRadians(aircraft['lat2'] - this.myAircraft['lat1']);
-            var Δλ = toRadians(aircraft['lon2'] - this.myAircraft['lon1']);
+            var φ1 = toRadians(this.myAircraft.latitude);
+            var φ2 = toRadians(aircraft.latitude);
+            var Δφ = toRadians(aircraft.latitude - this.myAircraft.latitude);
+            var Δλ = toRadians(aircraft.longitude - this.myAircraft.longitude);
 
             var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
                 Math.cos(φ1) * Math.cos(φ2) *
                 Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
             var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-            aircraft['distance'] = R * c;
+            aircraft.distance = R * c;
 
-            if (aircraft['distance'] > this.watchDistance) {
-                aircraft['flightZone'] = 'safe';
-            } else if (aircraft['distance'] > this.warningDistance) {
-                aircraft['flightZone'] = 'watch';
-            } else if (aircraft['distance'] > this.dangerDistance) {
-                aircraft['flightZone'] = 'warning';
+            if (aircraft.distance > this.watchDistance) {
+                aircraft.flightZone = 'safe';
+            } else if (aircraft.distance > this.warningDistance) {
+                aircraft.flightZone = 'watch';
+            } else if (aircraft.distance > this.dangerDistance) {
+                aircraft.flightZone = 'warning';
             } else {
-                aircraft['flightZone'] = 'danger';
+                aircraft.flightZone = 'danger';
             }
             return aircraft;
         });
@@ -60,3 +57,4 @@ export class ATAEngine {
 function toRadians(degrees: number): number {
     return degrees * Math.PI / 180;
 }
+
