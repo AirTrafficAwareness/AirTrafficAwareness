71dc2075dfdf8989f2600482bbe2988333bd50e1
TondiToday <tonditfk@hotmail.com>
2020-07-18 12:55:33 -0500

    Update to unit tests

--- Server/test/request.test.ts
+++ Server/test/request.test.ts
@@ -1,14 +1,14 @@
 import request, { RequestParams } from "../src/request"
 import assert from "assert"
-// describe("requests", () => {
-//     context("Postive Return", () => {
-//         it("should return a address not found", async function (){
-//             const requestParams: RequestParams = {
-//                 qs: {key: "test"},
-//                 auth: {username: "test", password: "test"}};
-//             const requests = request('https://www.test.com',requestParams);
-//             requests.then(res => res).catch(err => assert.equal(err, new SyntaxError('Unexpected token < in JSON at position 0')));
-//             });
-//         });
-//     });
+describe("requests", () => {
+    context("Postive Return", () => {
+        it("should return a address not found", async function (){
+            const requestParams: RequestParams = {
+                qs: {key: "test"},
+                auth: {username: "test", password: "test"}};
+            const requests = request('https://www.test.com',requestParams);
+            requests.then(res => res).catch(err => assert.equal(err, new SyntaxError('Unexpected token < in JSON at position 0')));
+            });
+        });
+    });
 
