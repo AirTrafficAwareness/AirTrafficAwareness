e2909bb523fafe361997bcaaa62ad851dd069370
Cameron Knight <cjk8zb@mail.umkc.edu>
2020-10-06 22:05:18 -0500

    Rework ATAService and ATAEngine to support GDL90 Service.

--- Client/src/app/ata.service.ts
+++ Client/src/app/ata.service.ts
@@ -2,6 +2,7 @@ import {EventEmitter, Injectable} from '@angular/core';
 import {Airplane, Coordinate} from './airplane';
 import {HttpClient} from '@angular/common/http';
 import {BehaviorSubject, Observable} from 'rxjs';
+import {ATAEngine} from './engine';
 
 const httpProtocol = `${location.protocol}`; // http: | https:
 const wsProtocol = httpProtocol.replace(/^http/, 'ws'); // ws: | wss:
@@ -12,18 +13,20 @@ const wsProtocol = httpProtocol.replace(/^http/, 'ws'); // ws: | wss:
 export class ATAService {
   public airplanes: BehaviorSubject<Airplane[]> = new BehaviorSubject([]);
   private webSocket;
-  private airplane: Airplane;
 
   get currentAirplane() {
-    return this.airplane;
+    if (this.engine.origin && 'identifier' in this.engine.origin) {
+      return this.engine.origin;
+    }
+    return undefined;
   }
 
   set currentAirplane(airplane: Airplane) {
     this.webSocket.send(JSON.stringify(airplane));
-    this.airplane = airplane;
+    this.engine.origin = airplane;
   }
 
-  constructor(public http: HttpClient) {}
+  constructor(public http: HttpClient, private engine: ATAEngine) {}
 
   connect(address: string, coordinate: Coordinate) {
     const qs = coordinate ? {
@@ -34,11 +37,20 @@ export class ATAService {
     return this.http.get<any>(`${httpProtocol}//${address}/api/`, {params: qs}).pipe(res => {
       const url = `${wsProtocol}//${address}`;
       const ws = new WebSocket(url);
+      this.engine.origin = coordinate;
       ws.onmessage = (message) => {
         const json = JSON.parse(message.data);
-        console.log('json', json);
         if (Array.isArray(json)) {
-          this.airplanes.next(json);
+          if (this.engine.origin && ('identifier' in this.engine.origin) && this.engine.origin.identifier) {
+            const origin: Airplane = this.engine.origin as Airplane;
+            const updated = json.find(airplane => airplane.identifier === origin.identifier);
+            if (updated) {
+              this.engine.origin = updated;
+            }
+          }
+          const a = json.map(airplane => this.engine.determineProximity(airplane));
+          console.log('processed airplanes', a);
+          this.airplanes.next(a);
         }
       };
       this.webSocket = ws;
--- Client/src/app/engine.ts
+++ Client/src/app/engine.ts
@@ -1,133 +1,133 @@
 import {Airplane, Coordinate, Point} from './airplane';
-import faaNumbers from "./NMASTER.json"
+import faaNumbers from '../assets/faaNumbers.json';
+import {Injectable} from '@angular/core';
 
 type CallbackFunction = (airplanes: Airplane[]) => void;
 
 interface FlightZones {
-    danger: number;
-    caution: number;
-    notice: number;
+  danger: number;
+  caution: number;
+  notice: number;
 }
 
+@Injectable({
+  providedIn: 'root'
+})
 export class ATAEngine {
-    public onGeneratedDistances: CallbackFunction = (() => {});
-    static origin: Airplane;
+  public origin: Coordinate | Airplane;
 
-    determineProximity(airplanes: Airplane[]): Airplane[] {
-        if (!ATAEngine.origin) {
-            return airplanes;
-        }
-
-        if (ATAEngine.origin.identifier) {
-            const updated = airplanes.find(airplane => airplane.identifier === ATAEngine.origin.identifier);
-            if (updated) {
-                ATAEngine.origin = updated;
-            }
-        }
-
-        const radius = toMeters(3); // Minimum horizontal separation is 3 nautical miles.
-        const flightZones = {
-            danger: radius,
-            caution: radius * 2,
-            notice: radius * 3,
-        };
-
-        airplanes.forEach(airplane => {
-            const distanceInMeters = calculateDistance(airplane, ATAEngine.origin);
-            if (Math.abs(airplane.latitude) > 90 || Math.abs(airplane.longitude) > 180) {
-                return;
-            }
-           let nNumber:string;
-           if(faaNumbers[airplane.identifier.toUpperCase()]){
-            nNumber = "N"+faaNumbers[airplane.identifier.toUpperCase()]; 
-           }
-            Object.assign(airplane, {
-                proximity: {
-                    distance: toNauticalMiles(distanceInMeters),
-                    flightZone: calculateFlightZone(distanceInMeters, flightZones),
-                    position: coordinateToPoint(airplane, ATAEngine.origin, flightZones.notice)
-                },
-                nNumber
-            });
-        });
-        //this.saveToFile('record.json',airplanes);
-        this.onGeneratedDistances(airplanes);
+  determineProximity(airplane: Airplane): Airplane {
+    if (faaNumbers[airplane.identifier.toUpperCase()]) {
+      const nNumber = 'N' + faaNumbers[airplane.identifier.toUpperCase()];
+      Object.assign(airplane, {
+        nNumber
+      });
     }
-    private saveToFile(fileName, airplanes):void {
-        const fs = require('fs');
-        const BSON = require('bson'); 
-        var stream = fs.createWriteStream(fileName, {flags:'a'});
-        const data = BSON.serialize(airplanes);
-        stream.write(data);
-        stream.close();
+
+    if (!this.origin) {
+      return airplane;
     }
+
+    const radius = toMeters(3); // Minimum horizontal separation is 3 nautical miles.
+    const flightZones = {
+      danger: radius,
+      caution: radius * 2,
+      notice: radius * 3,
+    };
+
+    const distanceInMeters = calculateDistance(airplane, this.origin);
+    if ('identifier' in this.origin && airplane.identifier === this.origin.identifier) {
+      console.log('Calculated ownship', distanceInMeters);
+    }
+    if (Math.abs(airplane.latitude) > 90 || Math.abs(airplane.longitude) > 180) {
+      return;
+    }
+    Object.assign(airplane, {
+      proximity: {
+        distance: toNauticalMiles(distanceInMeters),
+        flightZone: calculateFlightZone(distanceInMeters, flightZones),
+        position: coordinateToPoint(airplane, this.origin, flightZones.notice)
+      }
+    });
+    // this.saveToFile('record.json',airplanes);
+    return airplane;
+  }
+
+  // private saveToFile(fileName, airplanes):void {
+  //     const fs = require('fs');
+  //     const BSON = require('bson');
+  //     var stream = fs.createWriteStream(fileName, {flags:'a'});
+  //     const data = BSON.serialize(airplanes);
+  //     stream.write(data);
+  //     stream.close();
+  // }
 }
 
 function calculateDistance(coordinate: Coordinate, origin: Coordinate): number {
-    const meanEarthRadius = 6371e3; // meters
+  const meanEarthRadius = 6371e3; // meters
 
-    const latTo = toRadians(coordinate.latitude);
-    const latFrom = toRadians(origin.latitude);
-    const lonD = toRadians(coordinate.longitude - origin.longitude);
+  const latTo = toRadians(coordinate.latitude);
+  const latFrom = toRadians(origin.latitude);
+  const lonD = toRadians(coordinate.longitude - origin.longitude);
 
-    const { acos, cos, sin } = Math;
-    return acos(sin(latFrom) * sin(latTo) + cos(latFrom) * cos(latTo) * cos(lonD)) * meanEarthRadius;
+  const {acos, cos, sin} = Math;
+  return acos(sin(latFrom) * sin(latTo) + cos(latFrom) * cos(latTo) * cos(lonD)) * meanEarthRadius;
 }
 
 function calculateFlightZone(distance: number, flightZones: FlightZones): string {
-    const { danger, caution, notice } = flightZones;
-    if (distance <= danger) {
-        return 'danger';
-    }
+  const {danger, caution, notice} = flightZones;
+  if (distance <= danger) {
+    return 'danger';
+  }
 
-    if (distance <= caution) {
-        return 'caution';
-    }
+  if (distance <= caution) {
+    return 'caution';
+  }
 
-    if (distance <= notice) {
-        return 'notice';
-    }
+  if (distance <= notice) {
+    return 'notice';
+  }
 
-    return 'safe';
+  return 'safe';
 }
 
 function coordinateToPoint(coordinate: Coordinate, origin: Coordinate, radius: number): Point {
-    const userInterfaceRadius = 360; // points
-    const scale = userInterfaceRadius / radius;
+  const userInterfaceRadius = 360; // points
+  const scale = userInterfaceRadius / radius;
 
-    const xDistance = (coordinate.longitude - origin.longitude) * metersPerLongitudeAtLatitude(origin.latitude);
-    const yDistance = (coordinate.latitude - origin.latitude) * metersPerLatitudeAtLatitude(origin.latitude);
+  const xDistance = (coordinate.longitude - origin.longitude) * metersPerLongitudeAtLatitude(origin.latitude);
+  const yDistance = (coordinate.latitude - origin.latitude) * metersPerLatitudeAtLatitude(origin.latitude);
 
-    const x = userInterfaceRadius + xDistance * scale;
-    const y = userInterfaceRadius - yDistance * scale;
+  const x = userInterfaceRadius + xDistance * scale;
+  const y = userInterfaceRadius - yDistance * scale;
 
-    return { x, y };
+  return {x, y};
 }
 
 function toRadians(degrees: number): number {
-    return degrees * Math.PI / 180;
+  return degrees * Math.PI / 180;
 }
 
 function toNauticalMiles(meters: number): number {
-    return meters / 1852;
+  return meters / 1852;
 }
 
 function toMeters(nauticalMiles: number): number {
-    return nauticalMiles * 1852;
+  return nauticalMiles * 1852;
 }
 
 function metersPerLongitudeAtLatitude(latitude): number {
-    const radians = toRadians(latitude);
-    const { cos } = Math;
-    return 111415.13 * cos(radians)
-        - 94.55 * cos(3.0 * radians)
-        + 0.12 * cos(5.0 * radians);
+  const radians = toRadians(latitude);
+  const {cos} = Math;
+  return 111415.13 * cos(radians)
+    - 94.55 * cos(3.0 * radians)
+    + 0.12 * cos(5.0 * radians);
 }
 
 function metersPerLatitudeAtLatitude(latitude): number {
-    const radians = toRadians(latitude);
-    const { cos } = Math;
-    return 111132.09 - 566.05 * cos(2.0 * radians)
-        + 1.2 * cos(4.0 * radians)
-        - 0.002 * cos(6.0 * radians);
+  const radians = toRadians(latitude);
+  const {cos} = Math;
+  return 111132.09 - 566.05 * cos(2.0 * radians)
+    + 1.2 * cos(4.0 * radians)
+    - 0.002 * cos(6.0 * radians);
 }
