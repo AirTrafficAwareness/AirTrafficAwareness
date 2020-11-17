99a7d0c0f515fa351fa2e97a31c55e213da4aed4
Cameron Knight <cjk8zb@mail.umkc.edu>
2020-11-17 12:36:14 -0600

    Add iOS platform

---
+++ Client/ios/App/App/AppDelegate.swift
@@ -0,0 +1,164 @@
+import UIKit
+import Capacitor
+import SystemConfiguration.CaptiveNetwork
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    triggerLocalNetworkPrivacyAlert()
+    return true
+  }
+
+  func applicationWillResignActive(_ application: UIApplication) {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+  }
+
+  func applicationDidEnterBackground(_ application: UIApplication) {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    application.isIdleTimerDisabled = false
+  }
+
+  func applicationWillEnterForeground(_ application: UIApplication) {
+    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+  }
+
+  func applicationDidBecomeActive(_ application: UIApplication) {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    application.isIdleTimerDisabled = true
+  }
+
+  func applicationWillTerminate(_ application: UIApplication) {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  }
+
+  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    // Called when the app was launched with a url. Feel free to add additional processing here,
+    // but if you want the App API to support tracking app url opens, make sure to keep this call
+    return CAPBridge.handleOpenUrl(url, options)
+  }
+  
+  func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    // Called when the app was launched with an activity, including Universal Links.
+    // Feel free to add additional processing here, but if you want the App API to support
+    // tracking app url opens, make sure to keep this call
+    return CAPBridge.handleContinueActivity(userActivity, restorationHandler)
+  }
+
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesBegan(touches, with: event)
+
+    let statusBarRect = UIApplication.shared.statusBarFrame
+    guard let touchPoint = event?.allTouches?.first?.location(in: self.window) else { return }
+
+    if statusBarRect.contains(touchPoint) {
+      NotificationCenter.default.post(CAPBridge.statusBarTappedNotification)
+    }
+  }
+
+//  #if USE_PUSH
+//
+//  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+//    NotificationCenter.default.post(name: Notification.Name(CAPNotifications.DidRegisterForRemoteNotificationsWithDeviceToken.name()), object: deviceToken)
+//  }
+//
+//  func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+//    NotificationCenter.default.post(name: Notification.Name(CAPNotifications.DidFailToRegisterForRemoteNotificationsWithError.name()), object: error)
+//  }
+//
+//  #endif
+
+}
+
+/// Does a best effort attempt to trigger the local network privacy alert.
+///
+/// It works by sending a UDP datagram to the discard service (port 9) of every
+/// IP address associated with a broadcast-capable interface. This should
+/// trigger the local network privacy alert, assuming the alert hasn’t already
+/// been displayed for this app.
+///
+/// This code takes a ‘best effort’. It handles errors by ignoring them. As
+/// such, there’s guarantee that it’ll actually trigger the alert.
+///
+/// - note: iOS devices don’t actually run the discard service. I’m using it
+/// here because I need a port to send the UDP datagram to and port 9 is
+/// always going to be safe (either the discard service is running, in which
+/// case it will discard the datagram, or it’s not, in which case the TCP/IP
+/// stack will discard it).
+///
+/// There should be a proper API for this (r. 69157424).
+///
+/// For more background on this, see [Triggering the Local Network Privacy Alert](https://developer.apple.com/forums/thread/663768).
+func triggerLocalNetworkPrivacyAlert() {
+    let sock4 = socket(AF_INET, SOCK_DGRAM, 0)
+    guard sock4 >= 0 else {
+        print("no v4")
+        return
+    }
+    defer { close(sock4) }
+    let sock6 = socket(AF_INET, SOCK_DGRAM, 0)
+    guard sock6 >= 0 else {
+        print("no v6")
+        return
+    }
+    defer { close(sock6) }
+
+    let addresses = addressesOfDiscardServiceOnBroadcastCapableInterfaces()
+    var message = [UInt8]("!".utf8)
+    for address in addresses {
+        address.withUnsafeBytes { buf in
+            let sa = buf.baseAddress!.assumingMemoryBound(to: sockaddr.self)
+            let saLen = socklen_t(buf.count)
+            let sock = sa.pointee.sa_family == AF_INET ? sock4 : sock6
+            let err = sendto(sock, &message, message.count, MSG_DONTWAIT, sa, saLen)
+            print("Send To err: \(err)")
+        }
+    }
+}
+/// Returns the addresses of the discard service (port 9) on every
+/// broadcast-capable interface.
+///
+/// Each array entry is contains either a `sockaddr_in` or `sockaddr_in6`.
+private func addressesOfDiscardServiceOnBroadcastCapableInterfaces() -> [Data] {
+    var addrList: UnsafeMutablePointer<ifaddrs>? = nil
+    let err = getifaddrs(&addrList)
+    guard err == 0, let start = addrList else {
+        print("Failed err: \(err)")
+        return []
+    }
+    defer { freeifaddrs(start) }
+    return sequence(first: start, next: { $0.pointee.ifa_next })
+        .compactMap { i -> Data? in
+            guard
+                (i.pointee.ifa_flags & UInt32(bitPattern: IFF_BROADCAST)) != 0,
+                let sa = i.pointee.ifa_addr
+            else {
+                print("welp no good.")
+                return nil
+            }
+            var result = Data(UnsafeRawBufferPointer(start: sa, count: Int(sa.pointee.sa_len)))
+            switch CInt(sa.pointee.sa_family) {
+            case AF_INET:
+                result.withUnsafeMutableBytes { buf in
+                    let sin = buf.baseAddress!.assumingMemoryBound(to: sockaddr_in.self)
+                    sin.pointee.sin_port = UInt16(9).bigEndian
+                }
+            case AF_INET6:
+                result.withUnsafeMutableBytes { buf in
+                    let sin6 = buf.baseAddress!.assumingMemoryBound(to: sockaddr_in6.self)
+                    sin6.pointee.sin6_port = UInt16(9).bigEndian
+                }
+            default:
+                print("nada.")
+                return nil
+            }
+            return result
+        }
+}
